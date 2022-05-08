a4ddd16d00b0e0d819fcf8beab9608936fadc4c8
unknown <vedstandard@runbox.com>
2022-05-08 02:19:52 +0200

    vj5 final

--- minmaxmatchsticks.py
+++ minmaxmatchsticks.py
@@ -4,14 +4,12 @@ Created on Sat May  7 18:18:50 2022
 
 @author: Vedran
 """
-import random
 
 class game:
         
     def __init__(self):
         self.stanje = 11
         self.igraPrvi = True
-        self.zadnjiDvi = "Nitko"
         
     def __str__(self):
         red = ""
@@ -20,7 +18,7 @@ class game:
         else:
             red = "P2"
             
-        return "Broj Štapića: "+str(self.stanje)+" || Na Redu: "+red+" || Zadnji povukao 2: "+self.zadnjiDvi
+        return "Broj Štapića: "+str(self.stanje)+" || Na Redu: "+red
         
     def all_moves(self):        
         moves = []
@@ -35,47 +33,133 @@ class game:
         
         return moves
     
-    def move(self, igrac, akcija):
-        self.stanje = self.stanje - akcija
-        if akcija == 2:
-            if self.igraPrvi == True:
-                self.zadnjiDvi = "P1"
-            else:
-                self.zadnjiDvi = "P2"
+    def move(self, akcija):
+        self.stanje = self.stanje - akcija        
         self.igraPrvi = not self.igraPrvi
+        
+    def undo(self, akcija):
+        self.stanje = self.stanje + akcija        
+        self.igraPrvi = not self.igraPrvi
+        
+    def pobjednik(self):
+        if self.igraPrvi == True:
+            return "P2"
+        else:
+            return "P1"        
         
     def game_over(self):
         if self.stanje<2:
             return True
         else:
             return False       
-   
         
+counter = 0
+
+def minimax(igra):
+    global counter
+    counter += 1
+    if igra.game_over() == True:
+        if igra.pobjednik() == "P1":
+            return 100
+        elif igra.pobjednik() == "P2":
+            return -100
+        else:
+            return 0
+    if igra.igraPrvi == False:
+        maxv = -1000
+        for m in igra.all_moves():
+            igra.move(m)
+            v = minimax(igra)
+            igra.undo(m)
+            if v > maxv:
+                maxv = v
+        return maxv
+    else:
+        minv = 1000
+        for m in igra.all_moves():
+            igra.move(m)
+            v = minimax(igra)
+            igra.undo(m)
+            if v < minv:
+                minv = v
+        return minv
+   
+def play():
+    while game.game_over() == False:
+        print(game.__str__())    
+        try:
+            unos =  int(input("Da li povlačite jednu ili dvije: "))
+        except Exception:
+            unos = ""   
+        
+        if(unos == 2 or unos == 1):
+            if unos == 2:
+                game.move(2)
+            else:
+                game.move(1)
+                
+            if game.game_over():
+                print("Pobjednik: "+game.pobjednik())
+            else:            
+                i = minimax(game)
+                if i == 100:
+                    game.move(2)
+                else:
+                    game.move(1)
+                
+                if game.game_over():
+                    print("Pobjednik: "+game.pobjednik())
+        
+def perft(igra):
+    if igra.game_over() == True:
+        return 1
+    cnt = 1
+    for m in igra.all_moves():
+        igra.move(m)
+        cnt += perft(igra)
+        igra.undo(m)
+    return cnt
+
+def minimax_ab(igra, alpha, beta):
+    global counter
+    counter += 1
+    
+    if igra.game_over() == True:
+        if igra.pobjednik == "P1":
+            return 100
+        elif igra.pobjednik == "P2":
+            return -100
+        else:
+            return 0
+        
+    if igra.igraPrvi == False:
+        for m in igra.all_moves():
+            igra.move(m)
+            v = minimax_ab(igra, alpha, beta)
+            igra.undo(m)
+            if v > alpha:
+                alpha = v
+            if alpha >= beta:
+                break
+        return alpha
+    else:
+        for m in igra.all_moves():
+            igra.move(m)
+            v = minimax_ab(igra, alpha, beta)
+            igra.undo(m)
+            if v < beta:
+                beta = v
+            if alpha >= beta:
+                break
+        return beta
+
+def podrezivanje():
+    minimax_ab(game, -1000, 1000)
+    print("Sa podrezivanjem: ",counter)
+    print("Bez podrezivanja: ",perft(game))
+    
 game = game()
 
-while game.game_over() == False:
-    print(game.__str__())
-    unos =  int(input("Da li povlačite jednu ili dvije"))
-    
-    if(unos == 2 or unos == 1):
-        if unos == 2:
-            game.move(game.igraPrvi, 2)
-        else:
-            game.move(game.igraPrvi, 1)
-            
-        if game.game_over():
-            print("Pobjednik: "+game.zadnjiDvi)
-        else:            
-            i = random.randint(1,2)
-            game.move(game.igraPrvi, i)
-            if game.game_over():
-                print("Pobjednik: "+game.zadnjiDvi)
-        
-        
-        
-        
-    
-    
-        
+
     
     
